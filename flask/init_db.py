83f661632df8ee5f13e772501003288945fe0bbb
haruhiro26 <takaku200626haruhiro@gmail.com>
2025-09-09 11:42:51 +0900

    Delete flask directory

--- flask/init_db.py
+++
@@ -1,7 +0,0 @@
-from testapp import app, db
-from testapp.models.employee import Employee
-
-with app.app_context():
-    db.create_all()
-
-print("Database tables created!")
--- flask/testapp/config.py
+++
@@ -1,3 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://haruhiro:Test1470@testsqlserver.postgres.database.azure.com:5432/postgres'
-SQLALCHEMY_TRACK_MODIFICATIONS = True
